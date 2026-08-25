4bf83f3669621e56126bd9f7469ebc22cad88f23
Bokume2 <nagatomoyuu2@gmail.com>
2026-08-25 11:32:47 +0900

    fix: 1つのGroup内でTaskTagの名前がユニークになるよう制約追加

--- internal/database/schema/task_tag.go
+++ internal/database/schema/task_tag.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type TaskTag struct {
 	gorm.Model
-	Name    string
-	GroupID uint
+	Name    string  `gorm:"index:idx_name_in_group,unique"`
+	GroupID uint    `gorm:"index:idx_name_in_group,unique"`
 	Tasks   []*Task `gorm:"many2many:task_task_tags;"`
 }
