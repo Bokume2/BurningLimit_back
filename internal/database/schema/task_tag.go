93393f9a32f6fb3c631fd939c31aa68729ae77f0
Bokume2 <nagatomoyuu2@gmail.com>
2026-08-25 11:31:56 +0900

    fix: GroupからTaskTagへのhas_many関係を追加

--- internal/database/schema/task_tag.go
+++ internal/database/schema/task_tag.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type TaskTag struct {
 	gorm.Model
-	Name  string
-	Tasks []*Task `gorm:"many2many:task_task_tags;"`
+	Name    string
+	GroupID uint
+	Tasks   []*Task `gorm:"many2many:task_task_tags;"`
 }
